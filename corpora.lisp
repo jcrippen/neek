f9de4f086c0de764ef5ecc647b0c0052e3df7c47
James A. Crippen <jcrippen@gmail.com>
2018-12-15 11:43:07 -0500

    Add file vector to CORPUS class.

--- corpora.lisp
+++ corpora.lisp
@@ -6,16 +6,21 @@
 
 (in-package "NEEK")
 
+(defvar *current-corpus* nil)
+
 (defclass corpus ()
   ((entries :type (vector entry *)
             :initarg :entries
             :documentation "A vector of corpus entries.")
    (entnum  :type fixnum
             :initarg :entnum
-            :documentation "Number of entries in ENTRIES slot.")
+            :documentation "Number of entries in the corpus.")
+   (files   :type (vector file *)
+            :initarg :files
+            :documentation "A vector of files in the corpus.")
    (filenum :type fixnum
             :initarg :filenum
-            :documentation "Number of files in corpus."))
+            :documentation "Number of files in the corpus."))
   (:documentation
    "The top-level object that contains a corpus. The ENTRIES slot contains a
 vector of ENTRY instances, one for each entry in the corpus."))
@@ -29,7 +34,10 @@ ENTRY instances for each entry in the corpus."))
   (load-corpus (uiop:merge-pathnames* dir)))
 
 (defmethod load-corpus ((dir pathname))
-  (let ((files (mapcar #'make-file (list-corpus-files dir)))
+  (let ((flist (mapcar #'make-file (list-corpus-files dir)))
+        (files (make-array 0 :element-type 'file
+                             :adjustable t
+                             :fill-pointer 0))
         (filenum 0)
         (entnum 0)
         (entries (make-array 0 :element-type 'entry
@@ -37,14 +45,16 @@ ENTRY instances for each entry in the corpus."))
                                :fill-pointer 0)))
     ;; FIXME: Better error.
     (if (null files) (error "No corpus files found."))
-    ;; Cdr down the files, making entries and storing files in them as
-    ;; appropriate.
-    (loop :for f :in files
+    ;; CDR down the files list, storing each one in the file vector and adding
+    ;; it to an entry or making a new entry for it as appropriate.
+    (loop :for f :in flist
           :with ent                     ;current entry
           :when (not (null f))
-            :do (incf filenum)
+            :do
+               (vector-push-extend f files)
+               (incf filenum)
           :until (null f)
-          :do
+            :do
              (if (null ent)
                  ;; First time so make a new entry.
                  (progn (setf ent (make-entry-from-file f))
@@ -53,20 +63,21 @@ ENTRY instances for each entry in the corpus."))
                  (if (file-matches-entry-p f ent)
                      ;; Add to current entry.
                      (add-file-to-entry f ent)
+                     ;; Otherwise find a matching entry.
                      (let ((match (find-if (lambda (x)
                                              (file-matches-entry-p f x))
                                            entries :from-end t)))
                        (if (not (null match))
-                           ;; Earlier entry matches, so add file to it.
+                           ;; An earlier entry matches, so add file to it.
                            (add-file-to-entry f match)
                            ;; No matches so make a new entry from this file.
-                           (progn
-                             (setf ent (make-entry-from-file f))
-                             (incf entnum)
-                             (vector-push-extend ent entries)))))))
+                           (progn (setf ent (make-entry-from-file f))
+                                  (incf entnum)
+                                  (vector-push-extend ent entries)))))))
     ;; Now make a CORPUS instance and return it.
     (values
      (make-instance 'corpus
                     :entries entries
                     :entnum entnum
+                    :files files
                     :filenum filenum))))
